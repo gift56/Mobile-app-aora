d02cceca2cee0903dcef643f4b2d638569df08f1
Efe G <efeasiughu@gmail.com>
2024-04-15 19:03:45 -0400

    working on screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,37 @@
-import { Link } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora</Text>
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to home
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full items-center justify-center h-full px-4">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[130px] h-[130px]"
+          />
+
+          <Image
+            source={images.cards}
+            resizeMode="contain"
+            className="max-w-[380px] w-full h-[300px]"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibility with
+              <Text className="text-secondary-200"> Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm text-gray-100 mt-7 text-center font-pregular">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
