facc2fd6a60fc4e758d90d0b918bc288c2391942
Efe G <efeasiughu@gmail.com>
2024-04-15 16:50:05 -0400

    installed tailwind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,15 @@
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora</Text>
-      <Link href="/profile" style={{color:"blue"}}>Profile</Link>
+    <View className="w-full flex items-center justify-center">
+      <Text className="text-2xl">Aora</Text>
+      <Link href="/profile" style={{ color: "blue" }}>
+        Profile
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
