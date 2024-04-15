73db6405ceaee030f99612c8129e41f52e6929a8
Efe G <efeasiughu@gmail.com>
2024-04-15 19:24:35 -0400

    working on auth screen

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full min-h-[85vh] px-4 my-6"></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
