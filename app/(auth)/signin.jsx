545a44825fb9dec8e7cdbad9e4a2a1fd5b70aa03
Efe G <efeasiughu@gmail.com>
2024-04-15 19:28:01 -0400

    working on auth screen

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 
@@ -6,7 +6,14 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full min-h-[85vh] px-4 my-6"></View>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMethod="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white font-psemibold mt-10">Log in to Aora</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
