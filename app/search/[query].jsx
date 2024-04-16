2d88b32fefa95c55281aca9cc15649172f182299
Efe G <efeasiughu@gmail.com>
2024-04-16 08:15:09 -0400

    working on home page

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getSearchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+
+  const { data: posts, refetch } = useAppwrite(() => getSearchPosts(query));
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="Text-3xl text-white">{query}</Text>
+    </SafeAreaView>
   );
 };
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,29 +1,41 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 import { icons } from "../constants";
+import { router, usePathname } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const SearchInput = ({ placeholder }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState("");
 
   return (
     <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200  focus:!border-secondary items-center flex-row space-x-4">
       <TextInput
         className="flex-1 text-white font-pregular text-base h-full w-full"
-        value={value}
+        value={query}
         placeholder={placeholder}
         placeholderTextColor="#7b7b8b"
-        onChangeText={handleChangeText}
-        secureTextEntry={title === "Password" && !showPassword}
+        onChangeText={(e) => setQuery(e)}
       />
 
-      <TouchableOpacity onPress={() => setShowPassword((prev) => !prev)}>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert("Missing Query", "Search field is required");
+          }
+          if (pathname.startsWith("/search")) {
+            router.setParams({ query });
+          } else {
+            router.push(`/search/${query}`);
+          }
+        }}
+      >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
