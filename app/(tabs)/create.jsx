b9cbc3c33888ff010f39fba676f9711b75d06131
Efe G <efeasiughu@gmail.com>
2024-04-16 09:55:04 -0400

    working on create a video

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -32,7 +32,7 @@ const Create = () => {
     const result = await DocumentPicker.getDocumentAsync({
       type:
         selectType === "image"
-          ? ["image/png", "image/jpg"]
+          ? ["image/png", "image/jpg", "image/jpeg"]
           : ["video/mp4", "video/gif"],
     });
 
